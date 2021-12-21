fffc8436ab7a1be50276e4e94756118cbe9eb872
marongv <marongv@gmail.com>
2021-12-21 19:24:43 +0900

    나의 첫 커밋

---
+++ Day02/src/HelloWorld3.java
@@ -0,0 +1,21 @@
+// 자바의 소스코드 구조
+
+// 클래스 Block
+/*
+[public/private/protected/default] class 클래스명{
+	
+	//메소드
+	[public/private/protected/default] [static] 리턴자료형 메서드명(입력자료형1, 입력자료형2) {
+		
+	}
+	
+}
+default-> 내용이 없어도 된다는 의미
+*/
+
+public class HelloWorld3 { // class block
+	public static void main(String[] args) { // method block
+		System.out.println("Hello World"); // 실행문(command)
+	}
+
+}
