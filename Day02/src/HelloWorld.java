374fb355f4ff251a6048682a73917488c019ef27
marongv <marongv@gmail.com>
2021-12-24 18:59:21 +0900

    삭제된거 복구

---
+++ Day02/src/HelloWorld.java
@@ -0,0 +1,24 @@
+
+/**
+ * JavaDoc 주석
+ * @author TJ
+ *
+ */
+/*
+ * 여러줄 주석 넣는법
+ * 작성자: 박경란
+ * 작성일: 2021.12.16
+ * 내용: HelloWorld 출력
+ */
+
+//클래스 정의부 (한줄주석)
+public class HelloWorld {
+	
+	//어플리케이션 스타팅 포인트(메인메소드)
+	public static void main(String[] args) {
+		
+		//Hello World 출력
+		System.out.println("Hello World"); 
+	}
+
+}
