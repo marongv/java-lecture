574b8cbaf8d1a3af3b84c3516e78dace29e0c59e
marongv <marongv@gmail.com>
2021-12-26 15:41:31 +0900

    구구단 while연습

--- Day05/src/GuGuExample.java
+++ Day05/src/GuGuExample.java
@@ -10,24 +10,25 @@ public class GuGuExample {
 			
 			for(int y = 1; y <= 9; y++) {
 				System.out.println(x + "*" + y + "=" + (x*y));
-				//if(y == 3) break;
+				if(y == 3) break;
 			}
 			
-			//if(x == 5) break;
+			if(x == 5) break;
 		}
 		
 		//while문으로 바꾸기
 		
-		System.out.println("while 문으로 작성");
-		int x = 2, y = 1;
-		
-		while(true) {
-			while(y++ < 10) {
-				System.out.println(x + "*" + y + "=" + (x*y));
-			}
-			if(x <10) break;
-			x++;
-		}
+//		System.out.println("while 문으로 작성");
+//		int x = 2, y = 1;
+//		
+//		while(x <= 9) {
+//			while(y <= 9) {
+//				System.out.println(x + "*" + y + "=" + (x*y));
+//				y++;
+//			}
+//			y = 1;
+//			x++;
+//		}
 		
 		System.out.println("출력");
 		System.out.println("출력2");
