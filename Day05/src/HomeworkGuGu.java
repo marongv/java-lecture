d331626d8796d61973849e95f5092e9dc84e948b
marongv <marongv@gmail.com>
2021-12-26 15:43:00 +0900

    구구단가로출력

---
+++ Day05/src/HomeworkGuGu.java
@@ -0,0 +1,13 @@
+
+//-- 가로로 출력
+public class HomeworkGuGu {
+	public static void main(String[] args) {
+		for(int a=1; a<=9; a++) { 
+			for(int b=2; b<=9; b++) {
+				System.out.print(b + "*" + a + "=" + (b*a) + "\t" );
+			}
+			System.out.println(); // 줄바꿈
+		}
+	}
+
+}
