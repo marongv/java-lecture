13bb008342ef143e4664cf2eb3a5c332fc6b1b37
marongv <marongv@gmail.com>
2022-01-04 20:56:15 +0900

    클래스의 정의, 객체생성방법, 재귀메소드

---
+++ Day08/src/Application3.java
@@ -0,0 +1,69 @@
+
+public class Application3 {
+
+	public static void main(String[] args) {
+		//-- 객체를 생성하는 방법
+		MathExample math = new MathExample();
+		int sum = math.plus(10, 20);
+		System.out.println(sum);
+		
+		// stack 구조 Last In First Out (LIFO)
+		System.out.println(math.minus(math.plus(10, 20),100));
+		
+		//재귀메소드
+		System.out.println(math.fact(4));
+		
+	}
+	
+}
+
+//-- 1. 클래스 정의
+class MathExample {
+
+	/*
+	//-- 속성(필드)-------
+	
+	String name;
+	int age;
+	// ------------------
+	
+	
+	//-- 메소드들(행위)
+	//-- 메소드 문법
+	//-- [접근한정자] 리턴데이터타입 메소드명 (매개변수) {
+	//      행동 문단	
+	// }
+	
+	void sayHello(String name) {
+		System.out.println(name);
+	}
+	*/
+	
+	//void 는 리턴이 없는 메소드
+	void printName() {
+		System.out.println("안녕");
+	}
+	
+	// f(x,y) = (x+y)
+	int plus (int x, int y) {
+		return x + y;
+	}
+	
+	int minus(int x, int y) {
+		return x - y;
+	}
+	
+	
+	// 4! = 4*3*2*1
+	// 100! = 100*99*98*...*1;
+	public int fact(int n) {
+		if( n <= 1) {
+			return n;
+		}
+		else {
+			return fact(n-1) * n;
+		}
+	}	
+		
+	
+}
