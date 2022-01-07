2f55635613a7855376d038c93498f7e1ca291fe0
marongv <marongv@gmail.com>
2022-01-07 21:52:35 +0900

    내부클래스

---
+++ Day10/src/InnerClass.java
@@ -0,0 +1,18 @@
+
+public class InnerClass {
+	
+	class InnerInnerClass{
+		
+	}
+	
+	static class StaticInnerClass{
+		
+	}
+	
+	public static void main(String[] args) {
+		InnerClass cls = new InnerClass();
+		InnerClass.InnerInnerClass inCls = cls.new InnerInnerClass();
+		InnerClass.StaticInnerClass sCls = new InnerClass.StaticInnerClass();
+	}
+
+}
